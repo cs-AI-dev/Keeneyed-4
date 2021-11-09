fbffe834d0ee5d56f27cf7662c767aee7656fa57
Kedev <80370389+cs-AI-dev@users.noreply.github.com>
2021-11-09 09:29:27 -0600

    Update dv_preformattingRedprints.py

--- master/drivers/dv_preformattingRedprints.py
+++ master/drivers/dv_preformattingRedprints.py
@@ -9,3 +9,16 @@
 
 import sys
 import os
+import dv_neuralNetworking as nns
+
+class install:
+	class preformatted:
+		def keeneyed_4_agi(parent):
+			
+			# The namesake of this entire engine, the Keeneyed-4 general intelligence is
+			# designed to support 7 neural nets, each leading into a central cortex which
+			# controls the thought of the system. For a text-only interface,  only 
+			
+			print("[ke4_install] beginning preformatted installation of the Keeneyed-4 AGI system ...", end="")
+			parent.InputNeuralNetwork = nns.FeedforwardNeuralNetwork(neuronObjectsList = [
+				])
