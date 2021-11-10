dfcb9a8790e6f54227238c2687566e9312b04960
Kedev <80370389+cs-AI-dev@users.noreply.github.com>
2021-11-10 15:36:53 -0600

    Update dv_simulationEngine.py

--- master/drivers/dv_simulationEngine.py
+++ master/drivers/dv_simulationEngine.py
@@ -9,3 +9,25 @@
 
 import sys
 import os
+
+class universal:
+	class Item:
+		def __init__(item, parentSimulation, *functions, **properties): # Input functions need a parent arg
+			item.functions = []
+			for function in functions:
+				item.functions.append(function)
+				
+			item.properties = {}
+			for propertyName in properties.keys():
+				item.properties[propertyName] = properties[propertyName]
+				
+		def call(item, targetFunction, argsList=None):
+			if targetFunction in item.functions:
+				targetFunction(item, argsList)
+
+class axonometric:
+	class Block:
+		def __init__(block, 
+	class Layer:
+		def __init__(layer, *blockStrings):
+			
