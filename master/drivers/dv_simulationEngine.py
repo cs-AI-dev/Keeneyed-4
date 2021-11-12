103aec139944c99806b9fd9481cc56a39d0d865d
ke-1 <jacobbodell@gmail.com>
2021-11-12 14:49:11 -0600

    Update dv_simulationEngine.py

--- master/drivers/dv_simulationEngine.py
+++ master/drivers/dv_simulationEngine.py
@@ -16,18 +16,49 @@ class universal:
 			item.functions = []
 			for function in functions:
 				item.functions.append(function)
-				
+
 			item.properties = {}
 			for propertyName in properties.keys():
 				item.properties[propertyName] = properties[propertyName]
-				
+
 		def call(item, targetFunction, argsList=None):
 			if targetFunction in item.functions:
 				targetFunction(item, argsList)
 
+    class BlockId:
+        def __init__(bid, name, *functions, **properties):
+            bid.functions = []
+            for function in functions:
+                bid.functions.append(function)
+
+            bid.properties = {}
+            for propertyName in properties.keys():
+                bid.properties[propertyName] = properties[propertyName]
+
+        def call(bid, targetFunction, argsList=None):
+            if targetFunction in bid.functions:
+                targetFunction(item, argsList)
+
+class standardElements:
+    class bids:
+        air = BlockId("air", )
+
 class axonometric:
 	class Block:
-		def __init__(block, 
+		def __init__(block, blockId):
+            pass
+
 	class Layer:
-		def __init__(layer, *blockStrings):
-			
+		def __init__(layer, *blockStrings, euclideanZCoordinate=None):
+            layer.height = euclideanZCoordinate
+            xc = -1
+            yc = -1
+            layer.block = {}
+			for blockString in blockStrings:
+                xc += 1
+                layer.block[xc] = {}
+                for block in blockString:
+                    yc += 1
+                    layer.block[xc][yc]
+
+    class AxonometricSimulation:
