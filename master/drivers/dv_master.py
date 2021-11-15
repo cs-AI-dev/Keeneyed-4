57c5b0ea9d1d75e39cb70f16e9ecd1c34b8c7f04
Kedev <80370389+cs-AI-dev@users.noreply.github.com>
2021-11-15 13:07:12 -0600

    Update dv_master.py

--- master/drivers/dv_master.py
+++ master/drivers/dv_master.py
@@ -15,9 +15,15 @@ false = False
 
 class routinesObject:
 	def __init__(this):
-		pass
+		p_first = True
+		s_first = True
 
 	def Primary(this):
+		if this.p_first == True:
+			print("[ke4_master_driver] executing primary routine ...", end="")
+			this.p_first == False
+		else:
+			print("[ke4_master_driver] reloading primary routine ...", end="")
 		# Primary routine imports all drivers,
 		# which includes the APIs for neural
 		# networking and memory accessing.
@@ -25,8 +31,14 @@ class routinesObject:
 		import dv_neuralNetworking as nns
 		import dv_cacheGeneration as cg
 		import dv_preformattingRedprints as pfr
+		print("primary routine complete.")
 
 	def Secondary(this):
+		if this.s_first == True:
+			print("[ke4_master_driver] executing secondary routine ...", end="")
+			this.s_first = False
+		else:
+			print("[ke4_master_driver] reloading primary routine ...", end=""
 		# Secondary routine prepares the AGI
 		# class. Should be treated with caution!
 		# Also instantiates some other tools.
@@ -56,9 +68,12 @@ class routinesObject:
 							pfr.install.
 					else:
 						raise pfr.InvalidPreformat(f"'{preformattingRedprint}' is not a valid preformat.")
+				  
+			def terminate():
+				  
 
 executeRoutine = routinesObject()
 
-def LoadAGIs():
+def MasterRoutine():
 	executeRoutine.Primary()
 	executeRoutine.Secondary()
