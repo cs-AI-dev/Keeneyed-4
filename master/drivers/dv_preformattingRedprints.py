a72fea8ec5aa2c94db1eea914b64c9663aa8b7a7
Kedev <80370389+cs-AI-dev@users.noreply.github.com>
2021-11-18 13:03:35 -0600

    Update dv_preformattingRedprints.py

--- master/drivers/dv_preformattingRedprints.py
+++ master/drivers/dv_preformattingRedprints.py
@@ -23,6 +23,65 @@ class FormattingError(Exception):
 preformats = ["keeneyed", "sharpclawed", "ozymandias"]
 maximum = "sentinel_maximum"
 
+class function:
+	class keeneyed_4:
+		def PassFunction(evolvingArguments, standardArguments, activationFunction, parent):
+			return standardArguments
+
+		def SubjectPredicateDetection(evolvingArguments, standardArguments, activationFunction, parent):
+			pass
+
+		def TokenizeByNLTK(evolvingArguments, standardArguments, activationFunction, parent):
+			pass
+
+		def EmphasisLevelDetection(evolvingArguments, standardArguments, activationFunction, parent):
+			pass
+
+		def StartFinishTokenDetection(evolvingArguments, standardArguments, activationFunction, parent):
+			pass
+
+		def Keeneyed4Tokenization(evolvingArguments, standardArguments, activationFunction, parent):
+			pass
+		
+		def ToneDetection(evolvingArguments, standardArguments, activationFunction, parent):
+			pass
+		
+		def SentenceConstruction(evolvingArguments, standardArguments, activationFunction, parent):
+			pass
+		
+		def SyntacticLanguageConstruction(evolvingArguments, standardArguments, activationFunction, parent):
+			pass
+		
+		def ImplicitReturnTypeDetection(evolvingArguments, standardArguments, activationFunction, parent):
+			pass
+		
+		def ReverseKeeneyed4Tokenization(evolvingArguments, standardArguments, activationFunction, parent):
+			pass
+		
+		def ReverseNLTKTokenization(evolvingArguments, standardArguments, activationFunction, parent):
+			pass
+		
+		def ReverseImplicitReturnType(evolvingArguments, standardArguments, activationFunction, parent):
+			pass
+		
+		def PunctuationSynthesis(evolvingArguments, standardArguments, activationFunction, parent):
+			pass
+		
+		def StartEndTokenSynthesis(evolvingArguments, standardArguments, activationFunction, parent):
+			pass
+		
+		def WordListingSynthesis(evolvingArguments, standardArguments, activationFunction, parent):
+			pass
+		
+		def AssociatedOutputsSynthesis(evolvingArguments, standardArguments, activationFunction, parent):
+			pass
+		
+		def TextualOutputsSynthesis(evolvingArguments, standardArguments, activationFunction, parent):
+			pass
+		
+		def NonTextualOutputsSynthesis(evolvingArguments, standardArguments, activationFunction, parent):
+			pass
+
 class install:
 	class preformattedAGIs:
 		class versions:
@@ -30,9 +89,10 @@ class install:
 		def keeneyed(parent, version):
 			
 			# The namesake of this entire engine, the Keeneyed-4 general intelligence is
-			# designed to support 7 neural nets, each leading into a central cortex which
-			# controls the thought of the system. For a text-only interface,  only 2 NNs
-			# are functional
+			# designed to support several neural nets, each leading into a central cortex which
+			# controls the thought of the system. For a text-only interface, only 4 neural
+			# nets are functional, the input and output nets for the text interface and the
+			# simulacrum's memory drive.
 			
 			if not version in install.preformattedAGIs.versions.keeneyed:
 				raise InvalidVersionNumber(f"'{version}' is not a valid version for the Keeneyed AGI system.")
@@ -41,7 +101,10 @@ class install:
 				print("[ke4_install] beginning preformatted installation of the Keeneyed-4 AGI system ...", end="")
 				print(" > beginning neural network instantiation ...")
 				parent.TextInputNeuralNetwork = nns.FeedforwardNeuralNetwork(neuronObjectsList  = [
-					nns.neuron.input("text", 
+					nns.neuron.input("text",
+							 evolvingArgumentsDictionary = {},
+							 function = function.keeneyed_4.PassFunction,
+							 layer = 0,
 					])
 				parent.TextOutputNeuralNetwork = nns.FeedforwardNeuralNetwork(neuronObjectsList = [
 					])
