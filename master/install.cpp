5300fc2601fb0207a88e2e1ff627fa5eba5ef814
Kedev <80370389+cs-AI-dev@users.noreply.github.com>
2021-12-06 12:06:05 -0600

    Created installation C++ script
    Compiling this script and running it will allow you to install a KE4 environment

---
+++ master/install.cpp
@@ -0,0 +1,41 @@
+// Copyright 2021-2022 Jacob Bodell
+
+// Licensed with a unique EULA license.
+// This system may only be used in accordance
+// with its EULA agreement.
+
+// Please read and agree to the EULA in its entirety
+// before using this system.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Running this program, once compiled with an official
+// C++ 
+
+int main() {
+	cout << "     KEENEYED-4 ARTIFICIAL INTELLIGENCE AND SIMULATION ENGINE ENVIRONMENT SETUP     \n";
+	cout << "____________________________________________________________________________________\n";
+	cout << "                                                                                    \n";
+	cout << " This program is designed to allow You to begin proper installation of the System's \n";
+	cout << " software components. By using this program, even if You do not complete the proper \n";
+	cout << " installation of the System's software components, You a) agree to and b) are hence \n";
+	cout << " legally bound by the terms of the EULA which governs System's software components' \n";
+	cout << " use until the EULA's termination.                                                  \n";
+	cout << "                                                                                    \n";
+	cout << " If You agree with all terms and conditions described within the EULA, then You may \n";
+	cout << " proceed with the installation of the Keeneyed-4 engine environment.                \n";
+	cout << "                                                                                    \n";
+	cout << " If You do not agree with any of the terms or conditions described within the EULA, \n";
+	cout << " then You must immediately discontinue your use of this program and cannot continue \n";
+	cout << " with the proper installation of the System's software components.                  \n";
+	cout << "____________________________________________________________________________________\n";
+	cout << "                                                                                    \n";
+	cout << " Do you agree to the terms described within the System's software component's EULA ?\n";
+	cout << " (y\n) ";
+	
+	string response;
+	getline(cin, response);
+	
+}
